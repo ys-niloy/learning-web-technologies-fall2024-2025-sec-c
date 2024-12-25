82babdc7f9e47fd4fffaa79ddaee00dd1949bbd4
YS Niloy <115401989+ys-niloy@users.noreply.github.com>
2024-12-25 20:01:20 +0600

    retrieve id

--- Final_Lab_Task_1/view/edit.php
+++ Final_Lab_Task_1/view/edit.php
@@ -1,14 +1,37 @@
 <?php
     session_start();
+    require_once('../model/userModel.php');
+
     if(!isset($_COOKIE['status'])){
-        header('location: login.html');  
+        header('location: login.html'); 
     }
 
-    if(isset($_REQUEST['id'])){
+    if (isset($_REQUEST['id'])){
         echo $_REQUEST['id'];
     }
 
-    $user = ['id'=>2, 'username'=>'alamin', 'email'=>'alamin@aiub.edu', 'password'=>12];
+    $conn = getConnection();
+
+    if (!$conn) {
+        die("Connection failed: ");
+    }
+
+
+    $id = $_REQUEST['id'];          //retrieving id from userlist page
+
+    $sql = "SELECT id, username, email, password FROM users WHERE id = $id";
+    $result = mysqli_query($conn, $sql);
+
+    if($result->num_rows > 0)
+    {
+        $row = $result->fetch_assoc();
+        
+        $username = $row['username'];
+        $password = $row['password'];
+        $email = $row['email'];
+    }
+
+
 ?>
 
 <html>
@@ -17,11 +40,15 @@
 </head>
 <body>
         <h2>Edit User</h2>
-        <form method="post" action="../controller/update.php" enctype=""> 
-            Name: <input type="text" name="username" value="<?=$user['username']?>" /> <br>
-            password: <input type="password" name="password" value="<?=$user['password']?>" /><br>
-            Email: <input type="email" name="email" value="<?=$user['email']?>" /><br>
-            <input type="submit" name="submit" value="Submit" />
+        <a href='home.php'>Back </a> |
+        <a href='../controller/logout.php'>logout </a>
+        <br>
+        <form method="post" action="../controller/update.php" enctype="">
+            <input type="hidden" name="id" value="<?=$id?>">            
+            Username <input type="text" name="username" value="<?=$username?>" /> <br>
+            Password <input type="password" name="password" value="<?=$password?>" /> <br>
+            Email <input type="email" name="email" value="<?=$email?>" /> <br>
+            <input type="submit" name="submit" value="Update" />
         </form>
 </body>
 </html>
